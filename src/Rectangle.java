d80852e6d2482e63147168fe13251e8fea6fb790
compwron <wordymusic@gmail.com>
2013-04-11 21:22:39 -0500

    Passing test for a point not being in a rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,11 +1,21 @@
+import java.awt.*;
+
 public class Rectangle {
     private int area;
 
+    // TODO: refactor away this constructor
     public Rectangle(int length, int width) {
         this.area = length * width;
     }
 
+    public Rectangle(Point point, Point point1, Point point2, Point point3) {
+    }
+
     public int getArea() {
         return area;
+    }
+
+    public boolean contains(Point point) {
+        return false;
     }
 }
--- test/RectangleTest.java
+++ test/RectangleTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.awt.*;
+
+import static junit.framework.Assert.assertFalse;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -8,5 +11,11 @@ public class RectangleTest {
     public void rectangleKnowsItsArea(){
         Rectangle rectangle = new Rectangle(1, 2);
        assertThat(rectangle.getArea(), is(2));
+   }
+
+   @Test
+    public void rectangleDoesNotContainPointOutsideItself(){
+       Rectangle rectangle = new Rectangle(new Point(1, 2), new Point(1, 2), new Point(1, 2), new Point(1, 2));
+       assertFalse(rectangle.contains(new Point(3, 3)));
    }
 }
