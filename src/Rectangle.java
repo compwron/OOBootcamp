aa074ba4b8cdee6896a49e347ed37efc251154a8
compwron <wordymusic@gmail.com>
2013-04-11 21:10:02 -0500

    Passing test for summing areas of list of rectangles

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,7 +2,7 @@ public class Rectangle {
     private int area;
 
     public Rectangle(int length, int width) {
-        //To change body of created methods use File | Settings | File Templates.
+        this.area = length * width;
     }
 
     public int getArea() {
