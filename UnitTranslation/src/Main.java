6f43b51dbd4d5272afc9ed8b975206a5daa74b7c
compwron <wordymusic@gmail.com>
2013-04-18 21:58:44 -0500

    Printing all length conversions

--- UnitTranslation/src/Main.java
+++ UnitTranslation/src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("How many inches in 1 foot?");
-        Length oneFoot = new Length(LengthType.Feet, 1);
-        System.out.println(oneFoot.expressedIn(LengthType.Inches));
+        printAllLengthConversions();
+    }
 
+    private static void printAllLengthConversions() {
         for(LengthType startingLengthType : LengthType.values()){
-            for(LengthType endingLengthType : LengthType.values()){
-                Double countOfStartingInEnding = new Length(startingLengthType, 1).expressedIn(endingLengthType).getCount();
-                System.out.println("1 " + startingLengthType.name() + " in " + endingLengthType.name() + " is " + countOfStartingInEnding);
-            }
-        }
+           for(LengthType endingLengthType : LengthType.values()){
+               Double countOfStartingInEnding = new Length(startingLengthType, 1).expressedIn(endingLengthType).getCount();
+               System.out.println("1 " + startingLengthType.name() + " in " + endingLengthType.name() + " is " + countOfStartingInEnding);
+           }
+       }
     }
 }
