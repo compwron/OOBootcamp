8f29bbfdecab0b3fb237002d6a8b26277d6478e9
compwron <wordymusic@gmail.com>
2013-04-18 20:56:12 -0500

    printing all translations for Length in main

--- UnitTranslation/src/Main.java
+++ UnitTranslation/src/Main.java
@@ -3,5 +3,12 @@ public class Main {
         System.out.println("How many inches in 1 foot?");
         Length oneFoot = new Length(LengthType.Feet, 1);
         System.out.println(oneFoot.expressedIn(LengthType.Inches));
+
+        for(LengthType startingLengthType : LengthType.values()){
+            for(LengthType endingLengthType : LengthType.values()){
+                Double countOfStartingInEnding = new Length(startingLengthType, 1).expressedIn(endingLengthType).getCount();
+                System.out.println("1 " + startingLengthType.name() + " in " + endingLengthType.name() + " is " + countOfStartingInEnding);
+            }
+        }
     }
 }
