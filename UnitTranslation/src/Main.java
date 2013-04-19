e2a1c670e6260f426c705fed004b5225468a9c26
compwron <wordymusic@gmail.com>
2013-04-18 22:02:16 -0500

    made printAllVolumeConversions in Main and classes to make it compile

--- UnitTranslation/src/Main.java
+++ UnitTranslation/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+
         printAllLengthConversions();
+
+        printAllVolumeConversions();
+    }
+
+    private static void printAllVolumeConversions() {
+        for(VolumeType startingVolumeType : VolumeType.values()){
+            for(VolumeType endingVolumeType : VolumeType.values()){
+                Double countOfStartingInEnding = new Volume(startingVolumeType, 1).expressedIn(endingVolumeType).getCount();
+                System.out.println("1 " + startingVolumeType.name() + " in " + endingVolumeType.name() + " is " + countOfStartingInEnding);
+            }
+        }
     }
 
     private static void printAllLengthConversions() {
---
+++ UnitTranslation/src/Volume.java
@@ -0,0 +1,14 @@
+public class Volume {
+    private Double count;
+
+    public Volume(VolumeType startingVolumeType, double count) {
+    }
+
+    public Volume expressedIn(VolumeType endingVolumeType) {
+        return null;
+    }
+
+    public Double getCount() {
+        return count;
+    }
+}
