34fd8934584f5c6ede9cb85eeba85e8792811b3e
compwron <wordymusic@gmail.com>
2013-04-20 16:04:57 -0500

    Removing old version of prompt 2

--- MeasurementTranslation/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("How many inches in 1 foot?");
-        Double inchesInFoot = new Feet(1.0).translateTo(new Inches()).getCount();
-        System.out.println(inchesInFoot);
-    }
-}
