2d1d3724f3d0f51b09b6b7af63bb8ad7861e88c3
compwron <wordymusic@gmail.com>
2013-05-16 09:02:56 -0500

    refactoring tests

--- Collections/test/CollectionTest.java
+++ Collections/test/CollectionTest.java
@@ -13,31 +13,31 @@ public class CollectionTest {
 
     @Test
     public void shouldSee5AsLargestNumberInListWhichIsNotLargerThan6() throws InvalidCollectionsOperation {
-        Collection collection = new Collection(new Integer[]{3, 3, 5, 6, 8});
+        Collection collection = new Collection(3, 3, 5, 6, 8);
         assertThat(collection.getLargestUnder(6), is(5));
     }
 
     @Test
     public void shouldFindMaximumOfCollectionWhenCollectionHasMoreThanOneItem() throws InvalidCollectionsOperation {
-        Collection collection = new Collection(new Integer[]{1, 2, 3});
+        Collection collection = new Collection(1, 2, 3);
         assertThat(collection.max(), is(3));
     }
 
     @Test
     public void shouldReturnIntInCollectionAsMinWhenCollectionIsOneItemLong() throws InvalidCollectionsOperation {
-        Collection collection = new Collection(new Integer[]{1});
+        Collection collection = new Collection(1);
         assertThat(collection.min(), is(1));
     }
 
     @Test
     public void shouldReturnLowestIntegerInMultiIntegerCollectionWhenAskedMin() throws InvalidCollectionsOperation {
-        Collection collection = new Collection(new Integer[]{2, 3});
+        Collection collection = new Collection(2, 3);
         assertThat(collection.min(), is(2));
     }
 
     @Test
     public void shouldReturnLowestIntegerInCollectionIfAskedMinOfCollectionWithTwoEqualSmallestValues() throws InvalidCollectionsOperation {
-        Collection collection = new Collection(new Integer[]{3, 2, 2});
+        Collection collection = new Collection(3, 2, 2);
         assertThat(collection.min(), is(2));
     }
 
